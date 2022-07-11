ee6452c71ce9a0663af81ed5519b51a86c30b644
Barry Barrette <BarryBarrette@gmail.com>
2022-07-11 00:25:20 -0400

    Improving jsdoc of game update function

--- js/game.js
+++ js/game.js
@@ -12,8 +12,9 @@ const utils = require('./utils'); // https://i.imgur.com/kbh5eJZ.jpeg
 
 /**
  * Method that does the core game logic described above
- * @param boardState - A 2-dimensional array of 0s and 1s representing dead and alive, respectively
+ * @param boardState - A 2-dimensional array of 0s and 1s representing dead and alive cells, respectively
  * @type boardState number[][]
+ * @returns The next board state after the rules have been applied to each cell
  */
 function update(boardState) {
 
--- test/testGame.js
+++ test/testGame.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const game = require('../js/game');
 
 describe('Game Of Life Unit Tests', () => {
-    it('should first test', function () {
+    it('should ', function () {
 
     });
 
